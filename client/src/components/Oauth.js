481e1ec52a5636469723f9d2821445dab8eb8015
harshcvedi <105970898+harshcvedi@users.noreply.github.com>
2024-05-18 10:16:38 +0530

    Update Oauth.js

--- client/src/components/Oauth.js
+++ client/src/components/Oauth.js
@@ -13,7 +13,7 @@ export default function Oauth() {
         provider.setCustomParameters({ prompt: 'select_account' })
         try {
             const resultsFromGoogle = await signInWithPopup(auth, provider)
-            const res = await fetch('https://firstproject-main.onrender.com/api/auth/google', {
+            const res = await fetch('http://localhost:8080/api/auth/google', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
